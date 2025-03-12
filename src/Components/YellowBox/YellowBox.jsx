e004e2339f792fdca9af3bbfb9adb329d007a326
rabab003 <bintaliraq4@gmail.com>
2025-03-12 07:46:33 +0300

    7:46 pm

---
+++ src/Components/YellowBox/YellowBox.jsx
@@ -0,0 +1,13 @@
+import "./YellowBox.css"
+export default function YellowBox(){
+    return (
+        <>
+        <h1>simple question</h1>
+        <p>1+1 =?</p>
+
+        <input className="input" type="text" placeholder="write your answer here" />
+        
+        
+        </>
+    )
+}
