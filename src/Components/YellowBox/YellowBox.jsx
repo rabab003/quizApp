389046943e4506ad31c6fe01281d9ff4584ac1b8
rabab003 <bintaliraq4@gmail.com>
2025-03-14 20:50:41 +0300

    14 march

--- src/Components/YellowBox/YellowBox.jsx
+++ src/Components/YellowBox/YellowBox.jsx
@@ -1,12 +1,25 @@
-import "./YellowBox.css"
+import "./YellowBox.css";
+import { useState } from "react";
+
+
 export default function YellowBox(){
+    const [answer, setanswer] = useState("");
+
+    function handelvalue(event){
+        setanswer(event.target.value)
+    }
+
     return (
         <>
+        <div className="main">
         <h1>simple question</h1>
         <p>1+1 =?</p>
 
-        <input className="input" type="text" placeholder="write your answer here" />
+        <input onChange={handelvalue} value={answer} className="input" type="text" placeholder="write your answer here" />
+        <p className="theanswer">{answer}</p>
         
+        </div>
+
         
         </>
     )
