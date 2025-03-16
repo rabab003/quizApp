befa041b89edf93ddbc2903b20666ead4bc1d80b
rabab003 <bintaliraq4@gmail.com>
2025-03-16 23:18:11 +0300

    11:18 pm

--- src/Components/correctAnswer/CorrectAnswer.jsx
+++ src/Components/correctAnswer/CorrectAnswer.jsx
@@ -3,7 +3,7 @@ import './correctAnswer.css';
 export default function CorrectAnswer(){
     return(
         <>
-        <div>
+        <div className='answer-box'>
 
             <div className="icon"></div>
             <p>correct answer</p>
