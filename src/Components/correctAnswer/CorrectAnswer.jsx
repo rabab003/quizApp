f2f76a4643b63f1b0623d6caa12f06b749b2fe76
rabab003 <bintaliraq4@gmail.com>
2025-03-19 21:41:15 +0300

    19 mar

--- src/Components/correctAnswer/CorrectAnswer.jsx
+++ src/Components/correctAnswer/CorrectAnswer.jsx
@@ -1,11 +1,12 @@
 import './correctAnswer.css';
+import correct from '../../assets/4436481.png'
 
 export default function CorrectAnswer(){
     return(
         <>
         <div className='answer-box'>
 
-            <div className="icon"></div>
+            <div className="icon"><img src={correct}/></div>
             <p>correct answer</p>
         </div>
         </>
